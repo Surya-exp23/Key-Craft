459e6cad1d385430b963f112cfac2ab5c649ea57
Surendra Singh <surendrasinghrathore13057@gmail.com>
2026-02-12 13:08:49 +0530

    landing page style and video

---
+++ home-landing page/script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const video = document.querySelector('video');
+            
+    if (video) {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                video.play();
+            } 
+            else {
+                video.pause();
+                }
+            });
+        }, 
+        { threshold: 0.5 });
+
+        observer.observe(video.closest('section'));
+
+    }
+});
