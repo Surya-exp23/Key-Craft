c7c86c9d256b4af5620f89728b52539339f32f66
Surendra Singh <surendrasinghrathore13057@gmail.com>
2026-02-14 12:51:22 +0530

    added footer section with animated text and fixed the navbar width and blur issues with it and the arrow to navigate faster to the top section and html tag with scroll smooth

--- home-landing page/script.js
+++ home-landing page/script.js
@@ -42,3 +42,35 @@ function toggleDropdown(section) {
         
     }
 }
+
+
+// this is for the effect on which the blur must be applied when the scrol a bit
+
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    
+    if (window.scrollY > 50) {
+    
+        navbar.classList.add('backdrop-blur-md', 'bg-black/70');
+    } else {
+      
+        navbar.classList.remove('backdrop-blur-md', 'bg-black/70');
+    }
+});
+
+
+
+// this is where the arrow button works
+const scrollup = () => {
+  const scrollupElement = document.getElementById("scrollup");
+  if (window.scrollY >= 250) {
+    scrollupElement.classList.remove("-bottom-1/2");
+    scrollupElement.classList.add("bottom-4");
+  }
+  else {
+    scrollupElement.classList.add("-bottom-1/2");
+    scrollupElement.classList.remove("bottom-4");
+  }
+}
+
+window.addEventListener('scroll', scrollup);
