7a9c39599fbb5fe214d0864ca4ab374a0030a498
Surendra Singh <surendrasinghrathore13057@gmail.com>
2026-02-17 01:23:16 +0530

    minor adjustments and fixes

--- login_page/script.js
+++ login_page/script.js
@@ -12,3 +12,13 @@ toLogin.addEventListener("click", (e) => {
     e.preventDefault();
     mainCard.classList.remove("signup-active");
 });
+
+
+document.getElementById("loginid12").addEventListener("click", function () {
+    window.location.href = "../stats_dashboard/index.html";
+});
+
+
+document.getElementById("createacc12").addEventListener("click", function () {
+    window.location.href = "../stats_dashboard/index.html";
+});
